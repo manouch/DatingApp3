ec9cfc27d4f365a7853ce0722515de0e25d2bfd1
Manuchehr Ahmadkhonov <manoujon@gmail.com>
2022-06-14 08:31:33 -0400

    End of section 17

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -10,13 +10,15 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using AutoMapper;
 using API.Helpers;
+using API.SignalR;
 
 namespace API.Extensions
 {
-  public static class ApplicationServiceExtensions
+  public static class ApplicationServiceExtensions 
   {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
     {
+      services.AddSingleton<PresenceTracker>();
       services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
       services.AddScoped<ITokenService, TokenService>();
       services.AddScoped<IPhotoService, PhotoService>();
